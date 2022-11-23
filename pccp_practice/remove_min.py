ea6dd247c6504969b82e8fac5af52d08def0a6b4
Qkite <yeonjigu4@gmail.com>
2022-11-23 17:00:14 +0900

    프로그래머스 - 제일 작은 수 제거하기 - 최솟값 업데이트

--- pccp_practice/remove_min.py
+++ pccp_practice/remove_min.py
@@ -6,11 +6,27 @@ import numpy as np
 def solution(arr):
     arr1 = np.array(arr)
     if (len(arr) <= 1):
-        return np.array([-1])
+        return [-1]
     else:
-        print(min(arr1))
         arr1 = np.delete(arr1, np.where(arr1== min(arr1))[0])
-        return arr1
+        return arr1.tolist()
 
 
 print(solution([115,2,3,11,1]))
+
+
+def solution1(arr):
+    if (len(arr) <= 1):
+        return [-1]
+    else:
+        temp = arr[0]
+        # temp에 가장 작은 수가 들어갈 수 있도록 함
+        for i in range(1,len(arr)):
+            if (temp > arr[i]):
+                temp = arr[i]
+        arr.remove(temp)
+        print(temp)
+        return arr
+
+
+print(solution1([115,2,3,11,1]))
